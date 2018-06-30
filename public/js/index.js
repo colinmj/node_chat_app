5168914c925617dddf15eecef4242566a93a6675
Colin MJ <colinmj@telus.net>
2018-06-30 12:57:04 -0700

    Scroll to the bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,25 @@
 $(document).ready(function () {
+
+
+  function scrollToBottom() {
+    //selectors
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child')
+
+
+    //heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+      messages.scrollTop(scrollHeight);
+    }
+
+  }
+
   var socket = io();
 
   console.log(moment());
@@ -21,6 +42,7 @@ $(document).ready(function () {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 
 
   });
@@ -35,6 +57,7 @@ $(document).ready(function () {
       createdAt: formattedTime
     });
     $('#messages').append(html);
+    scrollToBottom();
 
   });
 
